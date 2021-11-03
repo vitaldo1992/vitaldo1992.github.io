7c0b2a6bf02232fdc77ba515d15a4ef94565df11
VitaliyOnRule <vitaliy.duvalko@rule.se>
2021-11-03 12:12:13 +0200

    images

--- bonnier-manage-preferences-sds/js/main.js
+++ bonnier-manage-preferences-sds/js/main.js
@@ -171,6 +171,7 @@ function load() {
                                                     preference.is_opted_in = pref.is_opted_in;
                                                     preference.position = pref.position;
                                                     preference.id = pref.id;
+                                                    preference.img = 'hanteranyhetsbrev.sydsvenskan.se' + preference.img // to remove
                                                 }
                                             })
                                         })
